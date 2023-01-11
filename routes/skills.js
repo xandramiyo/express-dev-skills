7b34209e9d3ddc9a5b5f00d4c080ace47fda3d28
xandramiyo <aj.sumiyoshi@gmail.com>
2023-01-10 16:33:12 -0800

    part 1 done

--- routes/skills.js
+++ routes/skills.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 const skillsCtrl = require('../controllers/skills');
 
-
-
-
 //GET /todos
 router.get('/', skillsCtrl.index)
 router.get('/:id', skillsCtrl.show)
